45f74e23f75c76637b95598133850ab7b4d74d45
Domenik Fox <domenik.fox@gmail.com>
2017-11-21 16:10:31 +0100

    Initial Commit

---
+++ BahnTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BahnTracker
+//
+//  Created by Domenik Fox on 21.11.17.
+//  Copyright © 2017 FoxCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
